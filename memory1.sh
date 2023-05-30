cc45a82c3709898a59665afbc2af3e593e08f982
mounika6893 <95367198+mounika6893@users.noreply.github.com>
2023-05-30 11:23:27 +0530

    Update memory1.sh
    second commit

--- memory1.sh
+++ memory1.sh
@@ -6,9 +6,10 @@ ALERT=90
 # sysadmin email ID
 ADMIN="mounika.matle@gmail.com" 
 
+# to get free space 
   usep=$(free | awk '/Mem/{printf("%.2f"), $3/$2 * 100}')
 
-  
+  # creating if statment to send mail 
   if [ $usep > $ALERT ]; then
     echo "Running out of space  on $(hostname) as on $(date)" |
     mail -s "Alert: Almost out of disk space $usep%" "$ADMIN"
